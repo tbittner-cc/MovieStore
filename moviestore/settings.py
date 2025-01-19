5982c1493cb1e2afbe8a01dfb6ca0f63a4616793
Coastal Chicago <166669036+tbittner-cc@users.noreply.github.com>
2025-01-19 20:52:56 +0000

    Created a basic database model.

--- moviestore/settings.py
+++ moviestore/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'home',
+    'movies',
 ]
 
 MIDDLEWARE = [
@@ -130,3 +131,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATICFILES_DIRS = [
     BASE_DIR / "moviestore/static",
 ]
+
+MEDIA_ROOT = BASE_DIR / "media"
+MEDIA_URL = "/media/"
